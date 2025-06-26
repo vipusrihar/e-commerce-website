a75e455b58851089296fee1ea3d286883d47fd04
vipusrihar <vipusrihar@gmail.com>
2025-06-26 17:47:02 +0530

    NavBar Created

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
-    <div>
-      <h1>Hello, React with Vite!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ react-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const HomePage = () => {
+  return (
+    <div className='p-0'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default HomePage
